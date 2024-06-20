cc6e44c9219c67c9cde47dd1c9430ef683cae3e2
vuficci <158140230+vuficci@users.noreply.github.com>
2024-06-21 01:55:09 +0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,31 @@
+DATABASE = {
+    'Серёга': 'Омск',
+    'Соня': 'Москва',
+    'Миша': 'Москва',
+    'Дима': 'Челябинск',
+    'Алина': 'Красноярск',
+    'Егор': 'Пермь',
+    'Коля': 'Красноярск'
+}
+
+def process_anfisa(query):
+    if query == 'Сколько у меня друзей?':
+        count = len(DATABASE)
+        return 'У тебя ' + str(count) + ' друзей.'
+    elif query == 'Кто все мои друзья?':
+        # Используем метод join() для создания строки из имен друзей
+        friends_string = ', '.join(DATABASE.keys())
+        return 'Твои друзья: ' + friends_string
+    elif query == 'Где все мои друзья?':
+        unique_cities = set(DATABASE.values())
+        # Используем метод join() для создания строки из названий городов
+        cities_string = ', '.join(unique_cities)
+        return 'Твои друзья в городах: ' + cities_string
+    else:
+        return '<неизвестный запрос>'
+
+
+print('Привет, я Анфиса!')
+print(process_anfisa('Сколько у меня друзей?'))
+print(process_anfisa('Кто все мои друзья?'))
+print(process_anfisa('Где все мои друзья?'))
